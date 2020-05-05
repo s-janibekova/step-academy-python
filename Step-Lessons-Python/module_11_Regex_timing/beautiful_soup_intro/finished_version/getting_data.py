b8f863552a036e7eebb0ca70a52f122df00631ad
s-janibekova <s.t.janibekova@gmail.com>
2020-05-06 03:06:09 +0600

    bs_regex_finished

---
+++ Step-Lessons-Python/module_11_Regex_timing/beautiful_soup_intro/finished_version/getting_data.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+page = requests.get('http://www.google.com')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+print(soup)
--- Step-Lessons-Python/module_11_Regex_timing/beautiful_soup_intro/html_parse.py
+++ Step-Lessons-Python/module_11_Regex_timing/beautiful_soup_intro/html_parse.py
@@ -17,13 +17,16 @@ SIMPLE_HTML = '''<html>
 
 simple_soup = BeautifulSoup(SIMPLE_HTML, 'html.parser')
 
+
 def find_title():
   print(simple_soup.find('h1').string)
+
 
 def find_list_items():
   list_items = simple_soup.find_all('li')
   list_content = [e.string for e in list_items]
   print(list_content)
+
 
 def find_paragraph():
   p = simple_soup.find('p', {'class': 'subtitle'}).string
@@ -36,13 +39,11 @@ def find_no_class_paragraph():
   print(other_paragraph[0].string)
 
 
-
-
 def main():
-  find_title()
-  find_list_items()
-  find_paragraph()
-  find_no_class_paragraph()
+  find_title( )
+  find_list_items( )
+  find_paragraph( )
+  find_no_class_paragraph( )
 
 
-main()
+main( )
